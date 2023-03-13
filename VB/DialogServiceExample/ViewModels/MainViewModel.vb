0881117ac24cec0d0cebc44c5b6aa6395230fa4c
DevExpressExampleBot <fakeEmail>
2023-03-13 19:43:06 +0400

    Source auto update [skip ci]

--- VB/DialogServiceExample/ViewModels/MainViewModel.vb
+++ VB/DialogServiceExample/ViewModels/MainViewModel.vb
@@ -1,12 +1,10 @@
 Imports DevExpress.Mvvm
-Imports System.Windows.Input
+Imports DevExpress.Mvvm.DataAnnotations
 
 Namespace DialogServiceExample.ViewModels
 
     Public Class MainViewModel
         Inherits ViewModelBase
-
-        Private _ShowDialogCommand As ICommand
 
         Public Property Result As MessageResult
             Get
@@ -18,26 +16,13 @@ Namespace DialogServiceExample.ViewModels
             End Set
         End Property
 
-        Public Property ShowDialogCommand As ICommand
-            Get
-                Return _ShowDialogCommand
-            End Get
-
-            Private Set(ByVal value As ICommand)
-                _ShowDialogCommand = value
-            End Set
-        End Property
-
         Protected ReadOnly Property DialogService As IDialogService
             Get
                 Return GetService(Of IDialogService)()
             End Get
         End Property
 
-        Public Sub New()
-            ShowDialogCommand = New DelegateCommand(AddressOf ShowDialog)
-        End Sub
-
+        <Command>
         Public Sub ShowDialog()
             Result = DialogService.ShowDialog(dialogButtons:=MessageButton.YesNoCancel, title:="Simple Dialog", viewModel:=New SimpleDialogViewModel())
         End Sub
--- VB/DialogServiceExample/ViewModels/SimpleDialogViewModel.vb
+++ VB/DialogServiceExample/ViewModels/SimpleDialogViewModel.vb
@@ -1,12 +1,10 @@
 Imports DevExpress.Mvvm
-Imports System.Windows.Input
+Imports DevExpress.Mvvm.DataAnnotations
 
 Namespace DialogServiceExample.ViewModels
 
     Public Class SimpleDialogViewModel
         Inherits ViewModelBase
-
-        Private _CloseCommand As ICommand
 
         Public Property DialogResult As MessageResult
             Get
@@ -18,27 +16,14 @@ Namespace DialogServiceExample.ViewModels
             End Set
         End Property
 
-        Public Property CloseCommand As ICommand
-            Get
-                Return _CloseCommand
-            End Get
-
-            Private Set(ByVal value As ICommand)
-                _CloseCommand = value
-            End Set
-        End Property
-
         Protected ReadOnly Property CurrentDialogService As ICurrentDialogService
             Get
                 Return GetService(Of ICurrentDialogService)()
             End Get
         End Property
 
-        Public Sub New()
-            CloseCommand = New DelegateCommand(Of MessageResult)(AddressOf Close)
-        End Sub
-
-        Public Sub Close(ByVal parameter As MessageResult)
+        <Command>
+        Public Sub Close()
             CurrentDialogService.Close(DialogResult)
         End Sub
     End Class
